e10a175cf2e78a50f1c733e7656d0044ee544bb2
Bruno-a472 <brunoangelo2015@hotmail.com>
2025-09-02 13:35:57 -0300

    build(spring): Muda toda a build para Springboot

---
+++ src/main/java/com/bruno/api_filmes_bruno/ApiFilmesBrunoApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.api_filmes_bruno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFilmesBrunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFilmesBrunoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bruno/api_filmes_bruno/ApiFilmesBrunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.api_filmes_bruno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFilmesBrunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
